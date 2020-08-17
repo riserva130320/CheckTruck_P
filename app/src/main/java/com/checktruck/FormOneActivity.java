57c76fb0d7e623b4f067f2224f61c3d7a2f2dfe4
Lammster <lamster1812@gmail.com>
2020-08-17 01:14:10 -0500

    addforms

---
+++ app/src/main/java/com/checktruck/FormOneActivity.java
@@ -0,0 +1,13 @@
+package com.checktruck;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class FormOneActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_form_one);
+    }
+}
