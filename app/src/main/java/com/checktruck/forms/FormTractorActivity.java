74c228f602bf20381441f96e5ba54eb5af89c7c5
Lammster <lamster1812@gmail.com>
2020-08-19 20:32:20 -0500

    add form tractor

---
+++ app/src/main/java/com/checktruck/forms/FormTractorActivity.java
@@ -0,0 +1,16 @@
+package com.checktruck.forms;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+import com.checktruck.R;
+
+public class FormTractorActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.avtivity_form_tractor);
+    }
+}
